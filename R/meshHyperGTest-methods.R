25aa17995bd416a4c8978292948e05bc7d2ada9a
k.tsuyuzaki <k.tsuyuzaki@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-11-03 17:52:16 +0000

    meshr-devel 1.3.1
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/meshr@96377 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/meshHyperGTest-methods.R
+++ R/meshHyperGTest-methods.R
@@ -3,117 +3,100 @@ setMethod("meshHyperGTest", signature(p="MeSHHyperGParams"),
 
 .meshHyperGTestInternal <- function(p) {
   ##
-  ## MeSH enrichment analysis 
+  ## Initialization
+  ##
+
+  ##
+  ## MeSH enrichment analysis
   ##
 
   ## Map gene ID to MeSH ID through annotation data
-  my.keytype <- c("GENEID")
-  my.cols <- c("GENEID", "MESHID")
-  my.geneids <- as.data.frame(p@geneIds)
-  names(my.geneids) <- "GENEID"
+  geneids <- as.data.frame(p@geneIds)
   universe.geneids <- as.data.frame(p@universeGeneIds)
+  names(geneids) <- "GENEID"
   names(universe.geneids) <- "GENEID"
+  selectedDatabase <- select(eval(parse(text=p@annotation)), keys = p@database, columns = c("GENEID", "MESHID","MESHCATEGORY", "SOURCEID"), keytype = "SOURCEDB")
+  selectedDatabase <- selectedDatabase[which(selectedDatabase[,3] == p@category), c(1:2,4)]
+  selectedDatabase <- as.data.frame(selectedDatabase)
+  selected.mesh <- table(merge(geneids, unique(selectedDatabase[,1:2]), "GENEID")[,2])
+  universe.mesh <- table(merge(universe.geneids, unique(selectedDatabase[,1:2]), "GENEID")[,2])
 
-  ## Retive data of specific database
-  selectedDatabase <- select(eval(parse(text=p@annotation)), keys = p@database, columns = c("GENEID", "MESHID","MESHCATEGORY"), keytype = "SOURCEDB")
-  selectedDatabase <- selectedDatabase[which(selectedDatabase[,3] == p@category), 1:2]
-
-  ## Error against impossible category-database combination was choosed
-  if(nrow(selectedDatabase) == 0){
-    stop("Impossible MeSH category - database combination was choosed.")
-  }
-
-  selected.mesh <- merge(my.geneids, selectedDatabase, "GENEID")
-  universe.mesh <- merge(universe.geneids, selectedDatabase, "GENEID")
-
-  selected.table <- table(selected.mesh[,2])
-  universe.table <- table(universe.mesh[,2])
-
-  ## Hypergeometric test 
-  pvals <- array()
-  for (i in 1:length(selected.table)) {
-    numWdrawn <- selected.table[i]
-    mesh.index <- which(names(selected.table[i])==names(universe.table))
-    numW <- universe.table[mesh.index]
+  ## Hypergeometric test
+  outputA <- data.frame()
+  length(outputA) <- 6
+  for(i in 1:length(selected.mesh)){
+    numWdrawn <- selected.mesh[i]
+    numW <- universe.mesh[which(names(selected.mesh[i])==names(universe.mesh))]
     numB <- length(p@universeGeneIds) - numW
     numDrawn <- length(p@geneIds)
-    pvals[i] <- phyper(numWdrawn - 1L, numW, numB, numDrawn, lower.tail=FALSE)
+    scores <- Category:::.doHyperGInternal(numW, numB, numDrawn, numWdrawn, over=T)
+    ## Assign statistics
+    outputA <- rbind(outputA, data.frame(names(selected.mesh[i]), as.numeric(scores$p), as.numeric(scores$odds), as.numeric(scores$expected), as.numeric(numWdrawn), as.numeric(numW), stringsAsFactors = F))
   }
-  
-  ## Multiple testing correction  
-  stats <- switch(p@pAdjust, BH = {
-    p.adjust(pvals, "BH")
-  }, QV = {
-    suppressWarnings(fdrtool(pvals, statistic="pvalue", plot=FALSE, verbose=FALSE)$qval)
-  }, lFDR = {
-    suppressWarnings(fdrtool(pvals, statistic="pvalue", plot=FALSE, verbose=FALSE)$lfdr)
-  }, none = pvals)
+  colnames(outputA) <- c("MESHID", "Pvalue", "OddsRatio", "ExpCount", "Count", "Size")
 
-  ## Retrieve full name of MeSH category 
-  mesh.cat <- p@category
+  ## Multiple testing correction
+  Pvalue <- outputA$Pvalue
 
-  switch(mesh.cat,
-    "A" = {
-      mesh.full.cat <- "Anatomy"
-    }, "B" = {
-      mesh.full.cat <- "Organisms"
-    }, "C" = {
-      mesh.full.cat <- "Diseases"
-    }, "D" = {
-      mesh.full.cat <- "Chemicals and Drugs"
-    }, "E" = {
-      mesh.full.cat <- "Analytical, Diagnostic and Therapeutic Techniques and Equipment"
-    }, "F" = {
-      mesh.full.cat <- "Psychiatry and Psychology"
-    }, "G" = {
-      mesh.full.cat <- "Phenomena and Processes"
-    }, "H" = {
-      mesh.full.cat <- "Disciplines and Occupations"
-    }, "I" = {
-      mesh.full.cat <- "Anthropology, Education, Sociology and Social Phenomena"
-    }, "J" = {
-      mesh.full.cat <- "Technology and Food and Beverages"
-    }, "K" = {
-      mesh.full.cat <- "Humanities"
-    }, "L" = {
-      mesh.full.cat <- "Information Science"
-    }, "M" = {
-      mesh.full.cat <- "Persons"
-    }, "N" = {
-      mesh.full.cat <- "Health Care"
-    }, "V" = {
-      mesh.full.cat <- "Publication Type"
-    }, "Z" = {
-      mesh.full.cat <- "Geographical Locations"
-    }
+  stats <- switch(p@pAdjust,
+    BH = {
+      p.adjust(Pvalue, "BH")},
+    QV = {
+      non_nan <- which(!is.nan(Pvalue))
+      pre_stats <- rep(NaN, length=length(Pvalue))
+      pre_stats[non_nan] <- fdrtool(Pvalue[non_nan], statistic="pvalue", plot=FALSE, verbose=FALSE)$qval
+      pre_stats},
+    lFDR = {
+      non_nan <- which(!is.nan(Pvalue))
+      pre_stats <- rep(NaN, length=length(Pvalue))
+      pre_stats[non_nan] <- fdrtool(Pvalue[non_nan], statistic="pvalue", plot=FALSE, verbose=FALSE)$lfdr
+      pre_stats},
+    none = {
+      Pvalue}
   )
 
-  ## Choose siginificantly enriched MeSH terms 
-  mesh.list <- names(selected.table[which(stats < p@pvalueCutoff)])
-  ## Create a data.frame
-  tmp.df <- data.frame("MESHID" = mesh.list, "PVALUE" = stats[which(stats < p@pvalueCutoff)], stringsAsFactors=FALSE)
-  ## Mapping  MeSH ID to MeSH term via MeSH.db
-  mesh.df <- select(MeSH.db, keys=mesh.list, columns=c("MESHID", "MESHTERM", "CATEGORY"), keytype="MESHID")
-  # remove categories not specified 
-  mesh.df <- mesh.df[mesh.cat==mesh.df[,3],][,c(1,2)]
-  ## merge mesh.df and tmp.df
-  mesh.df <- merge(mesh.df, tmp.df, by = "MESHID", all=TRUE)
-  ## calculate sorted index
-  sort.index <- unlist(sort(mesh.df[,3], index.return=TRUE)[2])
-  ## sort mesh.df based on p-values
-  mesh.df <- mesh.df[sort.index,]
-  # remove MeSH terms appearing multiple times within same category 
-  mesh.df <- mesh.df[!duplicated(mesh.df[,1]),]
-  if (nrow(mesh.df) != 0){
-    rownames(mesh.df) <- 1:nrow(mesh.df)
+  if(p@pAdjust != "none"){
+    outputA <- cbind(outputA, stats)
+    colnames(outputA)[which(colnames(outputA) == "stats")]  <- p@pAdjust
+    colnames(outputA) <- c("MESHID", "Pvalue", "OddsRatio", "ExpCount", "Count", "Size", p@pAdjust)
   }
-  
+
+  ## Choose siginificantly enriched MeSH terms
+  if(length(which(stats < p@pvalueCutoff)) != 0){
+      outputA <- outputA[which(stats < p@pvalueCutoff), ]
+    }else{
+      stop("None of MeSH Term is significant !")
+    }
+
+  FromMeSHdb <- select(MeSH.db, keys=names(selected.mesh), columns=c('MESHID', 'MESHTERM'), keytype='MESHID')
+  outputB <- merge(FromMeSHdb, selectedDatabase, by = "MESHID")
+  output <- merge(outputA, outputB, by = "MESHID")
+  output <- output[order(output$Pvalue), ]
+
+  ## Retrieve full name of MeSH category
+  switch(p@category,
+    "A" = {mesh.full.cat <- "Anatomy"},
+    "B" = {mesh.full.cat <- "Organisms"},
+    "C" = {mesh.full.cat <- "Diseases"},
+    "D" = {mesh.full.cat <- "Chemicals and Drugs"},
+    "E" = {mesh.full.cat <- "Analytical, Diagnostic and Therapeutic Techniques and Equipment"},
+    "F" = {mesh.full.cat <- "Psychiatry and Psychology"},
+    "G" = {mesh.full.cat <- "Phenomena and Processes"},
+    "H" = {mesh.full.cat <- "Disciplines and Occupations"},
+    "I" = {mesh.full.cat <- "Anthropology, Education, Sociology and Social Phenomena"},
+    "J" = {mesh.full.cat <- "Technology and Food and Beverages"},
+    "K" = {mesh.full.cat <- "Humanities"},
+    "L" = {mesh.full.cat <- "Information Science"},
+    "M" = {mesh.full.cat <- "Persons"},
+    "N" = {mesh.full.cat <- "Health Care"},
+    "V" = {mesh.full.cat <- "Publication Type"},
+    "Z" = {mesh.full.cat <- "Geographical Locations"}
+  )
+
   new("MeSHHyperGResult",
       meshCategory=mesh.full.cat,
-      meshAnnotation=p@annotation, 
-      meshDatabase=p@database, 
-      meshIds=mesh.df[,1],
-      meshTerms=mesh.df[,2],
-      pvalues=mesh.df[,3])
+      meshAnnotation=p@annotation,
+      meshDatabase=p@database,
+      ORA=as.data.frame(output)
+  )
 }
-
